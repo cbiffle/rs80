b4e8ff8b1703495988bf5b8463083e55a2d2d6ed
Cliff L. Biffle <code@cliffle.com>
2018-07-14 15:33:40 -0700

    Factor disassembler into library.

--- src/bin/disasm.rs
+++ src/bin/disasm.rs
@@ -4,8 +4,7 @@ use std::io::{self, Read};
 use std::env;
 use std::fs;
 
-use rs80::dis::Operand;
-use rs80::isa::RegM;
+use rs80::dis;
 
 fn main() -> io::Result<()> {
     let mut args = env::args();
@@ -15,62 +14,22 @@ fn main() -> io::Result<()> {
         Some(f) => f,
     };
 
-    let table = rs80::make_decode_table();
-
-    let mut file = fs::File::open(filename)?.bytes().enumerate()
-        .map(|(i, r)| (i + 0x100, r));
-    while let Some((addr, opcode)) = file.next() {
-        let opcode = opcode?;
-
+    let mut file = fs::File::open(filename)?.bytes();
+    let mut addr = 0x100;
+    loop {
         print!("{:04X}\t", addr);
-        match table[opcode as usize] {
-            None => println!("DB {:02X}", opcode),
-            Some(f) => {
-                let insn = f(rs80::isa::Opcode(opcode));
-                if let Some(cc) = insn.mnemonic.cc {
-                    print!("{}{:?}", insn.mnemonic.label, cc);
-                } else {
-                    print!("{}", insn.mnemonic.label);
-                }
-                if let Some(a) = insn.a {
-                    print!("\t");
-                    print_operand(a, &mut file)?;
-                    if let Some(b) = insn.b {
-                        print!(",");
-                        print_operand(b, &mut file)?;
-                    }
-                }
+        let out = io::stdout();
+        let mut out = out.lock();
+        match dis::disassemble(&mut file, &mut out)? {
+            0 => {
+                println!("END");
+                return Ok(())
+            },
+            n => {
+                addr += n;
                 print!("\n");
             },
         }
     }
-    Ok(())
 }
 
-fn print_operand(op: Operand,
-                 f: &mut impl Iterator<Item=(usize, io::Result<u8>)>)
-    -> io::Result<()>
-{
-    match op {
-        Operand::RM(RegM::R(r)) => print!("{:?}", r),
-        Operand::RM(m) => print!("{:?}", m),
-        Operand::RP(rp) => print!("{:?}", rp),
-        Operand::PSW => print!("PSW"),
-        Operand::C3(c) => print!("{:02X}H", c * 8),
-        Operand::I8 | Operand::Port8 => if let Some((_, i)) = f.next() {
-            print!("{:02X}H", i?)
-        } else {
-            print!("???")
-        },
-        Operand::I16 | Operand::Addr => if let Some((_, lo)) = f.next() {
-            if let Some((_, hi)) = f.next() {
-                print!("{:04X}H", (hi? as u16) << 8 | (lo? as u16))
-            } else {
-                print!("???")
-            }
-        } else {
-            print!("???")
-        },
-    }
-    Ok(())
-}
--- src/dis.rs
+++ src/dis.rs
@@ -1,4 +1,7 @@
-use super::isa::{CC, RegM, RegPair};
+use super::isa::{CC, RegM, RegPair, Opcode};
+use super::ops;
+
+use std::io::{self, prelude::*};
 
 #[derive(Copy, Clone, Debug)]
 pub enum Operand {
@@ -10,6 +13,16 @@ pub enum Operand {
     Port8,
     I16,
     Addr,
+}
+
+impl Operand {
+    fn addl_bytes(self) -> usize {
+        match self {
+            Operand::I8 | Operand::Port8 => 1,
+            Operand::I16 | Operand::Addr => 2,
+            _ => 0,
+        }
+    }
 }
 
 impl From<RegM> for Operand {
@@ -65,4 +78,73 @@ impl InsnInfo {
         InsnInfo { mnemonic: m.into(), a: Some(a.into()), b: None }
 
     }
+
+    pub fn len(&self) -> usize {
+        1 + self.a.map(|o| o.addl_bytes()).unwrap_or(0)
+          + self.b.map(|o| o.addl_bytes()).unwrap_or(0)
+    }
+}
+
+pub fn disassemble(bytes: &mut impl Iterator<Item = io::Result<u8>>,
+                   out: &mut impl Write) -> io::Result<usize> {
+    let table = ops::make_decode_table();
+    let opcode = match bytes.next() {
+        None => return Ok(0),
+        Some(op) => op?,
+    };
+
+    let mut n = 1;
+    match table[opcode as usize] {
+        None => write!(out, "DB {:02X}", opcode)?,
+        Some(f) => {
+            let insn = f(Opcode(opcode));
+            if let Some(cc) = insn.mnemonic.cc {
+                write!(out, "{}{:?}", insn.mnemonic.label, cc)?;
+            } else {
+                write!(out, "{}", insn.mnemonic.label)?;
+            }
+            if let Some(a) = insn.a {
+                write!(out, "\t")?;
+                n += print_operand(a, bytes, out)?;
+                if let Some(b) = insn.b {
+                    write!(out, ",")?;
+                    n += print_operand(b, bytes, out)?;
+                }
+            }
+        },
+    }
+
+    Ok(n)
+}
+
+fn print_operand(op: Operand,
+                 f: &mut impl Iterator<Item=io::Result<u8>>,
+                 out: &mut impl Write) -> io::Result<usize>
+{
+    match op {
+        Operand::RM(RegM::R(r)) => write!(out, "{:?}", r)?,
+        Operand::RM(m) => write!(out, "{:?}", m)?,
+        Operand::RP(rp) => write!(out, "{:?}", rp)?,
+        Operand::PSW => write!(out, "PSW")?,
+        Operand::C3(c) => write!(out, "{:02X}H", c * 8)?,
+        Operand::I8 | Operand::Port8 => if let Some(i) = f.next() {
+            write!(out, "{:02X}H", i?)?;
+            return Ok(1)
+        } else {
+            write!(out, "???")?;
+            return Ok(0)
+        },
+        Operand::I16 | Operand::Addr => if let Some(lo) = f.next() {
+            if let Some(hi) = f.next() {
+                write!(out, "{:04X}H", (hi? as u16) << 8 | (lo? as u16))?;
+                return Ok(2)
+            } else {
+                write!(out, "???")?;
+                return Ok(1)
+            }
+        } else {
+            write!(out, "???")?
+        },
+    }
+    return Ok(0)
 }
