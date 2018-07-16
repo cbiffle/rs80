934fdd3538d8a81b814264f3ede358e0df2d121b
Cliff L. Biffle <code@cliffle.com>
2018-07-15 18:24:38 -0700

    disasm: command line arguments

--- src/bin/disasm.rs
+++ src/bin/disasm.rs
@@ -1,26 +1,49 @@
 extern crate rs80;
+extern crate clap;
 
 use std::io::{self, Read};
-use std::env;
 use std::fs;
 
+use clap::{App,Arg};
 use rs80::dis;
 
 fn main() -> io::Result<()> {
-    let mut args = env::args();
-    args.next();
-    let filename = match args.next() {
-        None => panic!("filename argument missing"),
-        Some(f) => f,
-    };
+    let matches = App::new("disasm")
+        .version("0.1")
+        .author("Cliff L. Biffle <code@cliffle.com>")
+        .about("Basic 8080 disassembler")
+        .arg(Arg::with_name("org")
+             .long("org")
+             .value_name("ADDR")
+             .help("Sets assumed base address in hex (default: 0100)")
+             .takes_value(true))
+        .arg(Arg::with_name("INPUT")
+             .help("File to read (omit for standard input)"))
+        .get_matches();
 
-    let mut file = fs::File::open(filename)?.bytes();
-    let mut addr = 0x100;
+    let addr = matches.value_of("org").unwrap_or("0100");
+    let addr = match u16::from_str_radix(addr, 16) {
+        Ok(a) => a,
+        Err(_) => {
+            eprintln!("Bad value for --org: {}", addr);
+            std::process::exit(1)
+        }
+    };
+    let mut addr = addr as usize;
+
+    let stdin = io::stdin();
+
+    let input: Box<Read> = match matches.value_of("INPUT") {
+        Some(filename) => Box::new(fs::File::open(filename)?),
+        None => Box::new(stdin.lock()),
+    };
+    let mut input = input.bytes();
+
+    let out = io::stdout();
+    let mut out = out.lock();
     loop {
         print!("{:04X}\t", addr);
-        let out = io::stdout();
-        let mut out = out.lock();
-        match dis::disassemble(&mut file, &mut out)? {
+        match dis::disassemble(&mut input, &mut out)? {
             0 => {
                 println!("END");
                 return Ok(())
